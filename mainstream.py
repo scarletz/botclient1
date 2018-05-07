93b754c5a9f0cf64ba397ed7472daf89f30582b2
scarletz <ultimatescarlet@gmail.com>
2018-05-07 12:55:05 +0900

    tes

--- mainstream.py
+++ mainstream.py
@@ -25,8 +25,26 @@ def test():
   if ret!=0:
     return None
   
+  
+
+  f = open('../intermediate/splittedGpsLog/Line.csv', 'w')
+  f.write("LAT,LON,TIME,SPEED\n")
+  lonlat=r.text.split(":")
+  strs=lonlat[0]+","+lonlat[1]+",2018-05-11T04:53:05Z,0.335212\n"
+  f.write(strs)
+  f.close()
+
+
   os.system("3_segmentFrames.sh ..")
-  os.system("4_caffeTest.sh ..") 
+  os.system("4_caffeTest.sh ..")  
+  os.system("rm ../intermediate/analysisFramesGpsInfo.csv")
+
+  f = open('../intermediate/analysisFramesGpsInfo.csv', 'w')
+  f.write("NAME,LAT,LON,TIME1,TIME2\n")
+  lonlat=r.text.split(":")
+  strs="frame00000.jpg,"+lonlat[0]+","+lonlat[1]+",0,0\n"
+  f.write(strs)
+  f.close()
   return 0
 if __name__ == '__main__':
   for i in range(10):
