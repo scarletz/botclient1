3ee489117a5dbc68bb65f399708fddceba3f75ae
scarletz <ultimatescarlet@gmail.com>
2018-05-07 15:25:55 +0900

    tes

--- mainstream.py
+++ mainstream.py
@@ -20,32 +20,38 @@ def test():
     return None
   else:
     print "download image"
-  os.system("rm ../intermediate/analysisFrames/frame00000.jpg")
-  ret=os.system("mv image/1.jpg ../intermediate/analysisFrames/frame00000.jpg")
-  if ret!=0:
-    return None
-  
-  
 
-  f = open('../intermediate/splittedGpsLog/Line.csv', 'w')
+
+  f = open('../intermediate/splittedGpsLog/Dammy.csv', 'w')
   f.write("LAT,LON,TIME,SPEED\n")
   lonlat=r.text.split(":")
-  strs=lonlat[0]+","+lonlat[1]+",2018-05-11T04:53:05Z,0.335212\n"
+  strs=lonlat[0]+","+lonlat[1]+",2018-05-11T04:53:05Z,60.335212\n"
   f.write(strs)
+  strs=lonlat[0]+","+lonlat[1]+",2018-05-11T04:53:15Z,100.335212\n"
+  f.write(strs)
+#  strs=lonlat[0]+","+lonlat[1]+",2018-05-11T04:53:15Z,200.335212\n"
+#  f.write(strs)
   f.close()
-
+  os.system("2_filterVideo.sh ..")
+  os.system("sudo rm ../intermediate/analysisFrames/frame00000.jpg")
+  ret=os.system("sudo mv image/1.jpg ../intermediate/analysisFrames/frame00000.jpg")
+  if ret!=0:
+    return None
 
   os.system("3_segmentFrames.sh ..")
   os.system("4_caffeTest.sh ..")  
-  os.system("rm ../intermediate/analysisFramesGpsInfo.csv")
+#  os.system("rm ../intermediate/analysisFramesGpsInfo.csv")
 
-  f = open('../intermediate/analysisFramesGpsInfo.csv', 'w')
-  f.write("NAME,LAT,LON,TIME1,TIME2\n")
-  lonlat=r.text.split(":")
-  strs="frame00000.jpg,"+lonlat[0]+","+lonlat[1]+",0,0\n"
-  f.write(strs)
-  f.close()
+#  f = open('../intermediate/analysisFramesGpsInfo.csv', 'w')
+#  f.write("NAME,LAT,LON,TIME1,TIME2\n")
+#  lonlat=r.text.split(":")
+#  strs="frame00000.jpg,"+lonlat[0]+","+lonlat[1]+",0,0\n"
+#  f.write(strs)
+#  f.close()
+  os.system("5_estimateDamageLevel.sh ..")
+  os.system("6_createVisData.sh .. ../app")
   return 0
+
 if __name__ == '__main__':
   for i in range(10):
     ret=test()
